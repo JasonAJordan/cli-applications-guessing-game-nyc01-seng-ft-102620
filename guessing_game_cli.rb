271a24735772ed53f55953a95341e5cbec1b4851
Jason Jordan <jasonajordan0@gmail.com>
2020-10-10 14:41:42 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   random_num = 1 + rand(6)
   
   
-  puts "Please guess a number 1-6, enter 'exit' to return."
+  #puts "Please guess a number 1-6, enter 'exit' to return."
   input = gets.chomp
   
   if input == 'exit'
